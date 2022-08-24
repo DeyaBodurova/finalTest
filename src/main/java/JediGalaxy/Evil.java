9a4bc013f077d974f5a03242e8bf3a018f8fcbcd
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-24 17:47:25 +0300

    wip

--- src/main/java/JediGalaxy/Evil.java
+++ src/main/java/JediGalaxy/Evil.java
@@ -1,4 +1,14 @@
 package JediGalaxy;
 
 public class Evil {
+    public static void moveEvil(BattleField field, int evilRow, int evilCol) {
+        while (evilRow >= 0 && evilCol >= 0) {
+            if (field.isInBounds(evilRow, evilCol)) {
+                field.setValue(evilRow, evilCol, 0);
+            }
+            evilRow--;
+            evilCol--;
+        }
+    }
+
 }
