d9dd967e6b34935069a1af80e43be2deb0c2ca0b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-25 19:09:20 +0300

    wip

---
+++ src/main/java/Person1/Pet.java
@@ -0,0 +1,28 @@
+package Person1;
+
+public class Pet extends Creatures implements Birthable{
+    private String name;
+    private String birthDate;
+
+    public Pet(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return birthDate;
+    }
+}
