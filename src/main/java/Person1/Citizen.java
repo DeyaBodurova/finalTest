16469b500ab552ad814329b2580b3c6cfe023ff4
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-25 19:06:55 +0300

    wip

--- src/main/java/Person1/Citizen.java
+++ src/main/java/Person1/Citizen.java
@@ -1,4 +1,51 @@
 package Person1;
 
-public class Citizen {
+public class Citizen extends Creatures implements Person,Birthable,Identifiable {
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        setName(name);
+        setAge(age);
+        setId(id);
+        setBirthDate(birthDate);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
 }
